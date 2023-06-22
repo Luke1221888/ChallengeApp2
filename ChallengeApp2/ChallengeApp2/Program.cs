7058bc8dc950aad9e7e841153c5a317a9fec3e36
liy <lukszygula@gmail.com>
2023-06-22 21:52:57 +0200

    D4 zadanie

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,27 +1,14 @@
-﻿//imie
-string name = "Ewa";
-//wiek
-var age = 15;
-//zmienne z płcią
-var male = 1;
-var female = 2;
+﻿string name = "Ewa";
+int age = 33;
+bool isWomen = true;
 
-if (age == 25)
-{
-    if (female == 2)
-     Console.WriteLine("kobieta ponizej 30 lat");
-}
-else if (age == 33)
-{
-    if (name == "Ewa")
-     Console.WriteLine("Ewa lat 33");
-}
-if (age < 5)
-{   if (male == 1)
-    Console.WriteLine("Wiek mniejszy niz 5");
-}
-else if (age < 18)
-{
-    if (male == 1)
-        Console.WriteLine("Niepełnoletni mężczyzna");
-}
+if (name == "Ewa" && age == 33)
+
+    Console.WriteLine("EWA LAT 33");
+
+else if (!isWomen && age < 18)
+    
+    Console.WriteLine("NIEPELNOLETNI MEZCZYZNA");
+
+else if (isWomen == true && age <= 30)
+    Console.WriteLine("KOBIETA PONIZEJ 30 LAT");
